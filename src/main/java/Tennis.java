08197080ec3b01b2928be8c55ca0cc8bc8937ab9
indong2024 <transistordiod@gmail.com>
2024-04-24 00:39:19 +0200

    Refactoring Tennis class

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,20 +1,48 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Tennis {
     private int firstPlayerScoreTimes;
+    private int secondPlayerScoreTimes;
+    private Map<Integer, String> mapScores = new HashMap<>() {{
+        put(0, "0");
+        put(1, "15");
+        put(2, "30");
+        put(3, "40");
+    }};
 
-    public int score() {
-        HashMap<Integer, Integer> mapScores = new HashMap<Integer, Integer>() {{
-            put(1, 15);
-            put(2, 30);
-        }};
-        if(firstPlayerScoreTimes > 0){
-            return mapScores.get(firstPlayerScoreTimes);
+    public String score() {
+
+        if(isScoreDifferent()){
+            return scoreEvaluator();
         }
-        return 0;
+        if(isDeuce()) {
+            return "Deuce";
+        }
+        return sameScoreBetweenPayerAandPlayerB();
+    }
+
+    private boolean isDeuce() {
+        return firstPlayerScoreTimes >= 3;
+    }
+
+    private boolean isScoreDifferent() {
+        return firstPlayerScoreTimes != secondPlayerScoreTimes;
+    }
+
+    private String scoreEvaluator(){
+        return "Player A: " + mapScores.get(firstPlayerScoreTimes) + " / Player B: " + mapScores.get(secondPlayerScoreTimes);
+    }
+
+    private String sameScoreBetweenPayerAandPlayerB(){
+        return "Player A: " + mapScores.get(firstPlayerScoreTimes) + " / Player B: " + mapScores.get(secondPlayerScoreTimes);
     }
 
     public void firstPlayerScore() {
         firstPlayerScoreTimes++;
+    }
+
+    public void secondPlayerScore() {
+        secondPlayerScoreTimes++;
     }
 }
--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -1,30 +1,113 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 public class TennisTest {
 
     Tennis tennis = new Tennis();
 
     @Test
-    public void initializationShouldReturnZero (){
-        scoreShouldDisplay(0);
+    public void initializationShouldReturnZeroForAll (){
+        shouldDisplayFirstPlayerScores (0);
+        shouldDisplaySecondPlayerScores(0);
+        scoreShouldDisplay("Player A: 0 / Player B: 0");
     }
 
     @Test
+    @DisplayName("Test Player A scores 1 times and B  don't score")
     public void shouldReturnFifteen (){
-        tennis.firstPlayerScore();
-        scoreShouldDisplay(15);
+        shouldDisplayFirstPlayerScores (1);
+        scoreShouldDisplay("Player A: 15 / Player B: 0");
     }
 
     @Test
+    @DisplayName("Test Player A scores 2 times and B  don't score")
     public void shouldReturnThirty (){
-        tennis.firstPlayerScore();
-        tennis.firstPlayerScore();
-        scoreShouldDisplay(30);
+        shouldDisplayFirstPlayerScores (2);
+        scoreShouldDisplay("Player A: 30 / Player B: 0");
     }
 
-    private void scoreShouldDisplay (int expected) {
+    @Test
+    @DisplayName("Test Player A scores 3 times and B  don't score")
+    public void shouldReturnForty (){
+        shouldDisplayFirstPlayerScores (3);
+        scoreShouldDisplay("Player A: 40 / Player B: 0");
+    }
+
+    @Test
+    @DisplayName("Test Player A don't score  and B  1 times")
+    public void shouldReturnFifteen2 (){
+        shouldDisplaySecondPlayerScores(1);
+        scoreShouldDisplay("Player A: 0 / Player B: 15");
+    }
+
+    @Test
+    @DisplayName("Test Player A don't score  and B  2 times")
+    public void shouldReturnThirty2 (){
+        shouldDisplaySecondPlayerScores(2);
+        scoreShouldDisplay("Player A: 0 / Player B: 30");
+    }
+
+    @Test
+    @DisplayName("Test Player A don't score  and B  3 times")
+    public void shouldReturnForty2 (){
+        shouldDisplaySecondPlayerScores (3);
+        scoreShouldDisplay("Player A: 0 / Player B: 40");
+    }
+
+    @Test
+    @DisplayName("Test Player A scores 1 times and B  1 times")
+    public void shouldReturnFifteenForBoth (){
+        shouldDisplayFirstPlayerScores (1);
+        shouldDisplaySecondPlayerScores(1);
+        scoreShouldDisplay("Player A: 15 / Player B: 15");
+    }
+
+    @Test
+    @DisplayName("Test Player A scores 2 times and B  2 times")
+    public void shouldReturnThirtyForBoth (){
+        shouldDisplayFirstPlayerScores (2);
+        shouldDisplaySecondPlayerScores(2);
+        scoreShouldDisplay("Player A: 30 / Player B: 30");
+    }
+
+    @Test
+    @DisplayName("Test Player A scores 2 times and B 1 time")
+    public void testPlayerAScores2timesAndPlayerBScores1time() {
+        shouldDisplayFirstPlayerScores (2);
+        shouldDisplaySecondPlayerScores(1);
+        scoreShouldDisplay("Player A: 30 / Player B: 15");
+    }
+
+    @Test
+    @DisplayName("Test Player A scores 3 times and B  2 times")
+    public void testPlayerAScores3timesAndPlayerBScores2time() {
+        shouldDisplayFirstPlayerScores (3);
+        shouldDisplaySecondPlayerScores(2);
+        scoreShouldDisplay("Player A: 40 / Player B: 30");
+    }
+
+    @Test
+    @DisplayName("Test Deuce")
+    public void testShouldReturnDeuce() {
+        shouldDisplayFirstPlayerScores (3);
+        shouldDisplaySecondPlayerScores(3);
+        scoreShouldDisplay("Deuce");
+    }
+
+    private void scoreShouldDisplay (String expected) {
         Assert.assertEquals(expected , tennis.score());
+    }
+
+    private void shouldDisplayFirstPlayerScores (int times){
+        for (int i = 0; i < times; i++){
+            tennis.firstPlayerScore();
+        }
+    }
+    private void shouldDisplaySecondPlayerScores (int times){
+        for (int i = 0; i < times; i++){
+            tennis.secondPlayerScore();
+        }
     }
 
 
